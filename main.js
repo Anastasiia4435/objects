776d01991e197d6a0ea2b2a44a0bf755b9882956
Anastasiia4435 <107216310+Anastasiia4435@users.noreply.github.com>
2023-04-20 19:25:03 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let car = {
+    name: "Volkswagen",
+    speed: 200,
+    color: "grey"
+}
+
+let person = {
+    name: "Alex",
+    age: 13,
+    marks: [12, 11, 10, 9]
+}
+
+console.log(car);
+console.log()
